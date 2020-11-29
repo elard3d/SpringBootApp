07af1c36de25814d0c60461993bcc12d90e47ef0
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-28 19:35:05 -0500

    enviado

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,36 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.service.UserService;
 
 @Controller
 public class UserController {
-@GetMapping("/")
-public String index() {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	RoleRepository roleRpository;
+	
+	@GetMapping("/")
+	public String index() {
 
 		return "index";
 	}
 
-@GetMapping("/userForm")
-public String userForm() {
+	@GetMapping("/userForm")
+	public String userForm(Model model) {
+
+		model.addAttribute("userForm", new User());
+		model.addAttribute("roles", roleRpository.findAll());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("listTab","active");
 
 		return "user-form/user-view";
 	}
